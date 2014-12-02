3ae3636b567ce49a69586474a7a0d89cdcb35ec2
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-02 12:57:55 -0500

    cleaner tests

--- test/test-clip.js
+++ test/test-clip.js
@@ -1,11 +1,14 @@
 
 var test = require('tape');
-
 var clip = require('../src/clip');
+
 
 function belowX(p, x) { return p[0] < x; }
 function aboveX(p, x) { return p[0] > x; }
-function intersectX(p0, p1, x) { return [x, (x - p0[0]) * (p1[1] - p0[1]) / (p1[0] - p0[0]) + p0[1]]; }
+
+function intersectX(p0, p1, x) {
+    return [x, (x - p0[0]) * (p1[1] - p0[1]) / (p1[0] - p0[0]) + p0[1]];
+}
 
 var coords1 =
     [[0, 0], [50, 0], [50, 10], [20, 10], [20, 20], [30, 20], [30, 30],
@@ -13,12 +16,13 @@ var coords1 =
 
 var coords2 = [[0, 0], [50, 0], [50, 10], [0, 10]];
 
+
 test('clips polylines', function (t) {
 
-    var poly1 = {coords: coords1, type: 2, props: 1},
-        poly2 = {coords: coords2, type: 2, props: 2};
-
-    var clipped = clip([poly1, poly2], 10, 40, belowX, aboveX, intersectX);
+    var clipped = clip([
+        {coords: coords1, type: 2, props: 1},
+        {coords: coords2, type: 2, props: 2}
+    ], 10, 40, belowX, aboveX, intersectX);
 
     t.same(clipped, [
         {"coords":[[10,0],[40,0]],"type":2,"props":1},
@@ -35,10 +39,10 @@ test('clips polylines', function (t) {
 
 test('clips polygons', function (t) {
 
-    var poly1 = {coords: coords1, type: 3, props: 1},
-        poly2 = {coords: coords2, type: 3, props: 2};
-
-    var clipped = clip([poly1, poly2], 10, 40, belowX, aboveX, intersectX);
+    var clipped = clip([
+        {coords: coords1, type: 3, props: 1},
+        {coords: coords2, type: 3, props: 2}
+    ], 10, 40, belowX, aboveX, intersectX);
 
     t.same(clipped, [
         {"coords":[[10,0],[40,0],[40,10],[20,10],[20,20],[30,20],[30,30],[40,30],
