0b07aa65fb134b4c0f71ba96d0862a24005eb3af
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-03 18:23:44 -0500

    put the new simplification code in place
    Fixes #6
    Fixes #3

--- src/tilegeojson.js
+++ src/tilegeojson.js
@@ -1,12 +1,21 @@
 module.exports = tileGeoJSON;
 
-// var simplify = require('./simplify');
+var simplify = require('./simplify');
 var clip = require('./clip');
 
+var tileSize = 4096,
+    padding = 0.05, // padding on each side of tile in percentage
+    minPx = Math.round(-padding * tileSize),
+    maxPx = Math.round((1 + padding) * tileSize);
 
 function tileGeoJSON(geojson, maxZoom) {
 
-    var features = [];
+    if (maxZoom === undefined) maxZoom = 14;
+
+    console.time('preprocess features');
+
+    var features = [],
+        tolerance = 1 / (tileSize * Math.pow(2, maxZoom)); // simplify up to maxZoom
 
     for (var i = 0; i < geojson.features.length; i++) {
         var feature = geojson.features[i],
@@ -14,14 +23,14 @@ function tileGeoJSON(geojson, maxZoom) {
 
         if (geom.type === 'LineString') {
             features.push({
-                coords: geom.coordinates.map(transform),
+                coords: project(geom.coordinates, tolerance),
                 type: 2,
                 props: feature.properties
             });
 
         } else if (geom.type === 'Polygon' && geom.coordinates.length === 1) {
             features.push({
-                coords: geom.coordinates[0].map(transform),
+                coords: project(geom.coordinates[0], tolerance),
                 type: 3,
                 props: feature.properties
             });
@@ -30,13 +39,14 @@ function tileGeoJSON(geojson, maxZoom) {
             throw new Error('Unsupported GeoJSON type');
         }
     }
+    console.timeEnd('preprocess features');
 
-    var tiles = {};
-    var stats = {};
+    var tiles = {},
+        stats = {};
 
-    console.time('tile');
-    if (features && features.length) splitTile(stats, tiles, features, 0, 0, 0, 0, 0, 512, 512, maxZoom);
-    console.timeEnd('tile');
+    console.time('generate tiles');
+    if (features && features.length) splitTile(stats, tiles, features, 0, 0, 0, 0, 0, 1, 1, maxZoom);
+    console.timeEnd('generate tiles');
 
     console.log(stats);
 
@@ -49,91 +59,106 @@ function splitTile(stats, tiles, features, z, tx, ty, x1, y1, x2, y2, maxZoom) {
 
     var id = toID(z, tx, ty);
 
-    // var simplified = simplifyFeatures(features);
+    tiles[id] = transformFeatures(features, Math.pow(2, z), tx, ty);
 
-    // if (features.length === 1 && features[0].coords.length - simplified[0].coords.length <= 1) {
-    //     tiles[id] = features;
-    //     return;
-    // }
-
-    // if (coordsNumWithin(features, 20)) {
-    //     tiles[id] = features;
-    //     return;
-    // }
-
-    tiles[id] = features;
-
+    if (isClippedSquare(tiles[id])) return;
     if (z === maxZoom) return;
 
     var x = (x1 + x2) / 2,
         y = (y1 + y2) / 2,
-        p = 25,
+        p = (x2 - x1) * padding / 2,
 
         left  = clip(features, x1 - p, x + p, 0, intersectX),
         right = clip(features, x - p, x2 + p, 0, intersectX);
-
-    tx *= 2;
-    ty *= 2;
-    z++;
 
     if (left) {
         var tl = clip(left, y1 - p, y + p, 1, intersectY),
             bl = clip(left, y - p, y2 + p, 1, intersectY);
 
-        if (tl) splitTile(stats, tiles, doubleCoords(tl), z, tx, ty,     x1 * 2, y1 * 2, x * 2, y * 2, maxZoom);
-        if (bl) splitTile(stats, tiles, doubleCoords(bl), z, tx, ty + 1, x1 * 2, y * 2, x * 2, y2 * 2, maxZoom);
+        if (tl) splitTile(stats, tiles, tl, z + 1, tx * 2, ty * 2,     x1, y1, x, y, maxZoom);
+        if (bl) splitTile(stats, tiles, bl, z + 1, tx * 2, ty * 2 + 1, x1, y, x, y2, maxZoom);
     }
 
     if (right) {
         var tr = clip(right, y1 - p, y + p, 1, intersectY),
             br = clip(right, y - p, y2 + p, 1, intersectY);
 
-        if (tr) splitTile(stats, tiles, doubleCoords(tr), z, tx + 1, ty,     x * 2, y1 * 2, x2 * 2, y * 2, maxZoom);
-        if (br) splitTile(stats, tiles, doubleCoords(br), z, tx + 1, ty + 1, x * 2, y * 2, x2 * 2, y2 * 2, maxZoom);
+        if (tr) splitTile(stats, tiles, tr, z + 1, tx * 2 + 1, ty * 2,     x, y1, x2, y, maxZoom);
+        if (br) splitTile(stats, tiles, br, z + 1, tx * 2 + 1, ty * 2 + 1, x, y, x2, y2, maxZoom);
     }
 }
 
-function simplifyFeatures(features) {
-    var simplified = [];
+function transformFeatures(features, z2, tx, ty) {
+    var transformed = [];
 
     for (var i = 0; i < features.length; i++) {
-        var coords = features[i].coords,
-            simplifiedCoords = simplify(coords);
-
-        simplified.push({
-            coords: simplifiedCoords,
-            type: features[i].type,
-            props: features[i].props
+        var feature = features[i];
+        transformed.push({
+            coords: transform(feature.coords, feature.type, z2, tx, ty),
+            type: feature.type,
+            props: feature.props
         });
     }
-
-    return simplified;
+    return transformed;
 }
 
-function doubleCoords(features) {
-    for (var i = 0; i < features.length; i++) {
-        var coords = features[i].coords;
-        for (var j = 0; j < coords.length; j++) {
-            coords[j] = [coords[j][0] * 2, coords[j][1] * 2];
+// project original points, calculating simplification data along the way
+function project(lonlats, tolerance) {
+    var projected = [];
+    for (var i = 0; i < lonlats.length; i++) {
+        projected.push(projectPoint(lonlats[i]));
+    }
+    simplify(projected, tolerance);
+    return projected;
+}
+
+// simplify and transform projected coordinates for tile geometry
+function transform(points, type, z2, tx, ty) {
+    var newPoints = [],
+        tolerance = 1 / (tileSize * z2);
+
+    for (var i = 0, len = points.length; i < len; i++) {
+        var p = points[i];
+        // simplify, keeping points with significance > tolerance (plus 1st, last, and clip points on boundaries)
+        if (type === 1 || i === 0 || i === len - 1 || p[2] === -1 || p[2] > tolerance) {
+            newPoints.push(transformPoint(p, z2, tx, ty));
         }
     }
-    return features;
+    return newPoints;
 }
 
-function transform(p) {
-    var sin = Math.sin(p[1] * Math.PI / 180);
-    return [
-        512 * (p[0] / 360 + 0.5),
-        512 * (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI)
-    ];
+function projectPoint(p) {
+    var sin = Math.sin(p[1] * Math.PI / 180),
+        x = (p[0] / 360 + 0.5),
+        y = (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI);
+    return [x, y, 0];
+}
+
+function transformPoint(p, z2, tx, ty) {
+    var x = Math.round(tileSize * (p[0] * z2 - tx)),
+        y = Math.round(tileSize * (p[1] * z2 - ty));
+    return [x, y];
 }
 
 function toID(z, x, y) {
     return (((1 << z) * y + x) * 32) + z;
 }
 
-function intersectX(a, b, x) { return [x, (x - a[0]) * (b[1] - a[1]) / (b[0] - a[0]) + a[1]]; }
-function intersectY(a, b, y) { return [(y - a[1]) * (b[0] - a[0]) / (b[1] - a[1]) + a[0], y]; }
+function intersectX(a, b, x) { return [x, (x - a[0]) * (b[1] - a[1]) / (b[0] - a[0]) + a[1], -1]; }
+function intersectY(a, b, y) { return [(y - a[1]) * (b[0] - a[0]) / (b[1] - a[1]) + a[0], y, -1]; }
+
+function isClippedSquare(features) {
+    if (features.length > 1) return false;
+    var feature = features[0];
+    if (feature.type !== 3) return false;
+
+    for (var i = 0; i < feature.coords.length; i++) {
+        var p = feature.coords[i];
+        if (p[0] !== minPx && p[0] !== maxPx) return false;
+        if (p[1] !== minPx && p[1] !== maxPx) return false;
+    }
+    return true;
+}
 
 function coordsNum(features) {
     var num = 0;
--- test/test-linestring.js
+++ test/test-linestring.js
@@ -2,15 +2,18 @@
 var tileGeoJSON = require('../src/tilegeojson.js');
 var route = require('./fixtures/route.json');
 
-// var test = {
-//     "type": "FeatureCollection",
-//     "features": [{
-//         "type": "Feature",
-//         "geometry": {
-//             "type": "LineString",
-//             "coordinates": [[0, 0], [10, 10], [20, 10], [25, 5], [30, -10]]
-//         }
-//     }]
-// };
+var test = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-180, -85], [-180, 85], [180, 85], [180, -85], [-180, -85]]]
+        }
+    }]
+};
 
-var tiles = tileGeoJSON(route, 24);
+var tiles = tileGeoJSON(route, 14);
+
+// var keys = Object.keys(tiles);
+// console.log(tiles[keys[Math.round(keys.length / 2)]][0].coords);
