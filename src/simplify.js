f8ddc6fde7560d81466e890c3ad441d15b234cf4
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-03 17:08:30 -0500

    change simplify to save point significance instead of actual simplification

--- src/simplify.js
+++ src/simplify.js
@@ -1,6 +1,40 @@
 
 module.exports = simplify;
 
+// calculate simplification data using optimized Douglas-Peucker algorithm
+
+function simplify(points, tolerance) {
+
+    var sqTolerance = tolerance * tolerance,
+        len = points.length,
+        first = 0,
+        last = len - 1,
+        stack = [],
+        i, maxSqDist, sqDist, index;
+
+    while (last) {
+
+        maxSqDist = 0;
+
+        for (i = first + 1; i < last; i++) {
+            sqDist = getSqSegDist(points[i], points[first], points[last]);
+
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
+            }
+        }
+
+        if (maxSqDist > sqTolerance) {
+            points[index][2] = maxSqDist;
+            stack.push(first, index, index, last);
+        }
+
+        last = stack.pop();
+        first = stack.pop();
+    }
+}
+
 // square distance between 2 points
 function getSqDist(a, b) {
 
@@ -37,71 +71,4 @@ function getSqSegDist(p, a, b) {
     dy = py - y;
 
     return dx * dx + dy * dy;
-}
-
-// simplification using optimized Douglas-Peucker algorithm with recursion elimination
-function simplifyDouglasPeucker(points, sqTolerance) {
-
-    var len = points.length,
-        markers = new Uint8Array(len),
-        first = 0,
-        last = len - 1,
-        stack = [],
-        newPoints = [],
-        i, maxSqDist, sqDist, index;
-
-    markers[first] = markers[last] = 1;
-
-    while (last) {
-
-        maxSqDist = 0;
-
-        for (i = first + 1; i < last; i++) {
-            sqDist = getSqSegDist(points[i], points[first], points[last]);
-
-            if (sqDist > maxSqDist) {
-                index = i;
-                maxSqDist = sqDist;
-            }
-        }
-
-        if (maxSqDist > sqTolerance) {
-            markers[index] = 1;
-            stack.push(first, index, index, last);
-        }
-
-        last = stack.pop();
-        first = stack.pop();
-    }
-
-    for (i = 0; i < len; i++) {
-        if (markers[i]) newPoints.push(points[i]);
-    }
-
-    return newPoints;
-}
-
-function coord(p) {
-    var sin = Math.sin(p[1] * Math.PI / 180),
-        x = (p[0] / 360 + 0.5),
-        y = (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI);
-    return [x, y, 1, 0];
-}
-
-function tileCoord(p, z2, tx, ty) {
-    var x = Math.round(4096 * (p[0] * z2 - tx)),
-        y = Math.round(4096 * (p[1] * z2 - ty));
-    return [x, y];
-}
-
-function simplify(points, tolerance, highestQuality) {
-
-    if (points.length <= 2) return points;
-
-    var sqTolerance = tolerance !== undefined ? tolerance * tolerance : 1;
-
-    points = highestQuality ? points : simplifyRadialDist(points, sqTolerance);
-    points = simplifyDouglasPeucker(points, sqTolerance);
-
-    return points;
 }
