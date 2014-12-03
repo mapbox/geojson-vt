126c8fe62137f0559779cc61f7d817afbaea680d
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-03 00:24:05 -0500

    better formatting, move util to bottom

--- src/tilegeojson.js
+++ src/tilegeojson.js
@@ -3,17 +3,6 @@ module.exports = tileGeoJSON;
 var simplify = require('./simplify');
 var clip = require('./clip');
 
-function transform(p) {
-    var sin = Math.sin(p[1] * Math.PI / 180);
-    return [
-        512 * (p[0] / 360 + 0.5),
-        512 * (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI)
-    ];
-}
-
-function toID(z, x, y) {
-    return (((1 << z) * y + x) * 32) + z;
-}
 
 function tileGeoJSON(geojson, maxZoom) {
 
@@ -54,59 +43,6 @@ function tileGeoJSON(geojson, maxZoom) {
     return tiles;
 }
 
-function intersectX(p0, p1, x) {
-    return [x, (x - p0[0]) * (p1[1] - p0[1]) / (p1[0] - p0[0]) + p0[1]];
-}
-
-function intersectY(p0, p1, y) {
-    return [(y - p0[1]) * (p1[0] - p0[0]) / (p1[1] - p0[1]) + p0[0], y];
-}
-
-function coordsNum(features) {
-    var num = 0;
-    for (var i = 0; i < features.length; i++) {
-        num += features[i].coords.length;
-    }
-    return num;
-}
-
-function coordsNumWithin(features, k) {
-    var num = 0;
-    for (var i = 0; i < features.length; i++) {
-        num += features[i].coords.length;
-        if (num > k) return false;
-    }
-    return true;
-}
-
-
-function simplifyFeatures(features) {
-    var simplified = [];
-
-    for (var i = 0; i < features.length; i++) {
-        var coords = features[i].coords,
-            simplifiedCoords = simplify(coords);
-
-        simplified.push({
-            coords: simplifiedCoords,
-            type: features[i].type,
-            props: features[i].props
-        });
-    }
-
-    return simplified;
-}
-
-function doubleCoords(features) {
-    for (var i = 0; i < features.length; i++) {
-        var coords = features[i].coords;
-        for (var j = 0; j < coords.length; j++) {
-            coords[j] = [coords[j][0] * 2, coords[j][1] * 2];
-        }
-    }
-    return features;
-}
-
 function splitTile(stats, tiles, features, z, tx, ty, x1, y1, x2, y2, maxZoom) {
 
     stats[z] = (stats[z] || 0) + 1;
@@ -136,20 +72,88 @@ function splitTile(stats, tiles, features, z, tx, ty, x1, y1, x2, y2, maxZoom) {
         left  = clip(features, x1 - p, x + p, 0, intersectX),
         right = clip(features, x - p, x2 + p, 0, intersectX);
 
+    tx *= 2;
+    ty *= 2;
+    z++;
 
     if (left) {
-        var topLeft    = clip(left, y1 - p, y + p, 1, intersectY),
-            bottomLeft = clip(left, y - p, y2 + p, 1, intersectY);
+        var tl = clip(left, y1 - p, y + p, 1, intersectY),
+            bl = clip(left, y - p, y2 + p, 1, intersectY);
 
-        if (topLeft)    splitTile(stats, tiles, doubleCoords(topLeft),     z + 1, tx * 2, ty * 2,     x1 * 2, y1 * 2, x * 2, y * 2, maxZoom);
-        if (bottomLeft) splitTile(stats, tiles, doubleCoords(bottomLeft),  z + 1, tx * 2, ty * 2 + 1, x1 * 2, y * 2, x * 2, y2 * 2, maxZoom);
+        if (tl) splitTile(stats, tiles, doubleCoords(tl), z, tx, ty,     x1 * 2, y1 * 2, x * 2, y * 2, maxZoom);
+        if (bl) splitTile(stats, tiles, doubleCoords(bl), z, tx, ty + 1, x1 * 2, y * 2, x * 2, y2 * 2, maxZoom);
     }
 
     if (right) {
-        var topRight    = clip(right, y1 - p, y + p, 1, intersectY),
-            bottomRight = clip(right, y - p, y2 + p, 1, intersectY);
+        var tr = clip(right, y1 - p, y + p, 1, intersectY),
+            br = clip(right, y - p, y2 + p, 1, intersectY);
 
-        if (topRight)    splitTile(stats, tiles, doubleCoords(topRight),    z + 1, tx * 2 + 1, ty * 2,     x * 2, y1 * 2, x2 * 2, y * 2, maxZoom);
-        if (bottomRight) splitTile(stats, tiles, doubleCoords(bottomRight), z + 1, tx * 2 + 1, ty * 2 + 1, x * 2, y * 2, x2 * 2, y2 * 2, maxZoom);
+        if (tr) splitTile(stats, tiles, doubleCoords(tr), z, tx + 1, ty,     x * 2, y1 * 2, x2 * 2, y * 2, maxZoom);
+        if (br) splitTile(stats, tiles, doubleCoords(br), z, tx + 1, ty + 1, x * 2, y * 2, x2 * 2, y2 * 2, maxZoom);
     }
 }
+
+function simplifyFeatures(features) {
+    var simplified = [];
+
+    for (var i = 0; i < features.length; i++) {
+        var coords = features[i].coords,
+            simplifiedCoords = simplify(coords);
+
+        simplified.push({
+            coords: simplifiedCoords,
+            type: features[i].type,
+            props: features[i].props
+        });
+    }
+
+    return simplified;
+}
+
+function doubleCoords(features) {
+    for (var i = 0; i < features.length; i++) {
+        var coords = features[i].coords;
+        for (var j = 0; j < coords.length; j++) {
+            coords[j] = [coords[j][0] * 2, coords[j][1] * 2];
+        }
+    }
+    return features;
+}
+
+function transform(p) {
+    var sin = Math.sin(p[1] * Math.PI / 180);
+    return [
+        512 * (p[0] / 360 + 0.5),
+        512 * (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI)
+    ];
+}
+
+function toID(z, x, y) {
+    return (((1 << z) * y + x) * 32) + z;
+}
+
+function intersectX(p0, p1, x) {
+    return [x, (x - p0[0]) * (p1[1] - p0[1]) / (p1[0] - p0[0]) + p0[1]];
+}
+
+function intersectY(p0, p1, y) {
+    return [(y - p0[1]) * (p1[0] - p0[0]) / (p1[1] - p0[1]) + p0[0], y];
+}
+
+function coordsNum(features) {
+    var num = 0;
+    for (var i = 0; i < features.length; i++) {
+        num += features[i].coords.length;
+    }
+    return num;
+}
+
+function coordsNumWithin(features, k) {
+    var num = 0;
+    for (var i = 0; i < features.length; i++) {
+        num += features[i].coords.length;
+        if (num > k) return false;
+    }
+    return true;
+}
+
