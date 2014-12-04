f3bf11a23f0b7e1354039021a3c4c9b0b834ed97
Vladimir Agafonkin <agafonkin@gmail.com>
2014-12-04 16:13:52 -0500

    more refactoring, separate transform code

--- src/tilegeojson.js
+++ src/tilegeojson.js
@@ -4,12 +4,13 @@ module.exports = tileGeoJSON;
 
 var clip = require('./clip');
 var convert = require('./convert');
+var transform = require('./transform');
 
-var tileSize = 4096,
-    simplification = 1,
+var extent = 4096,
+    tolerance = 1 / extent, // simplification tolerance
     padding = 0.05, // padding on each side of tile in percentage
-    minPx = Math.round(-padding * tileSize),
-    maxPx = Math.round((1 + padding) * tileSize);
+    minPx = Math.round(-padding * extent),
+    maxPx = Math.round((1 + padding) * extent);
 
 function tileGeoJSON(geojson, maxZoom) {
 
@@ -18,10 +19,10 @@ function tileGeoJSON(geojson, maxZoom) {
     console.time('preprocess features');
 
     var features = [],
-        tolerance = simplification / (tileSize * Math.pow(2, maxZoom)); // simplify up to maxZoom
+        z2 = Math.pow(2, maxZoom); // simplify up to maxZoom
 
     for (var i = 0; i < geojson.features.length; i++) {
-        features.push(convert(geojson.features[i], tolerance));
+        features.push(convert(geojson.features[i], tolerance / z2));
     }
     console.timeEnd('preprocess features');
 
@@ -29,7 +30,7 @@ function tileGeoJSON(geojson, maxZoom) {
         stats = {};
 
     console.time('generate tiles');
-    if (features && features.length) splitTile(stats, tiles, features, 0, 0, 0, 0, 0, 1, 1, maxZoom);
+    if (features && features.length) splitTile(stats, tiles, features, 0, 1, 0, 0, 0, 0, 1, 1, maxZoom);
     console.timeEnd('generate tiles');
 
     console.log(stats);
@@ -37,10 +38,10 @@ function tileGeoJSON(geojson, maxZoom) {
     return tiles;
 }
 
-function splitTile(stats, tiles, features, z, tx, ty, x1, y1, x2, y2, maxZoom) {
+function splitTile(stats, tiles, features, z, z2, tx, ty, x1, y1, x2, y2, maxZoom) {
 
     var id = toID(z, tx, ty),
-        tile = transformFeatures(features, Math.pow(2, z), tx, ty);
+        tile = transform(features, z2, tx, ty, tolerance, extent);
 
     if (isClippedSquare(tile)) return; // useless tile
 
@@ -60,62 +61,18 @@ function splitTile(stats, tiles, features, z, tx, ty, x1, y1, x2, y2, maxZoom) {
         var tl = clip(left, y1 - p, y + p, 1, intersectY),
             bl = clip(left, y - p, y2 + p, 1, intersectY);
 
-        if (tl) splitTile(stats, tiles, tl, z + 1, tx * 2, ty * 2,     x1, y1, x, y, maxZoom);
-        if (bl) splitTile(stats, tiles, bl, z + 1, tx * 2, ty * 2 + 1, x1, y, x, y2, maxZoom);
+        if (tl) splitTile(stats, tiles, tl, z + 1, z2 * 2, tx * 2, ty * 2,     x1, y1, x, y, maxZoom);
+        if (bl) splitTile(stats, tiles, bl, z + 1, z2 * 2, tx * 2, ty * 2 + 1, x1, y, x, y2, maxZoom);
     }
 
     if (right) {
         var tr = clip(right, y1 - p, y + p, 1, intersectY),
             br = clip(right, y - p, y2 + p, 1, intersectY);
 
-        if (tr) splitTile(stats, tiles, tr, z + 1, tx * 2 + 1, ty * 2,     x, y1, x2, y, maxZoom);
-        if (br) splitTile(stats, tiles, br, z + 1, tx * 2 + 1, ty * 2 + 1, x, y, x2, y2, maxZoom);
+        if (tr) splitTile(stats, tiles, tr, z + 1, z2 * 2, tx * 2 + 1, ty * 2,     x, y1, x2, y, maxZoom);
+        if (br) splitTile(stats, tiles, br, z + 1, z2 * 2, tx * 2 + 1, ty * 2 + 1, x, y, x2, y2, maxZoom);
     }
 }
-
-function transformFeatures(features, z2, tx, ty) {
-    var transformed = [];
-
-    for (var i = 0; i < features.length; i++) {
-        var feature = features[i];
-
-        transformed.push({
-            geometry: transform(feature.geometry, feature.type, z2, tx, ty),
-            type: feature.type,
-            tags: feature.tags || null
-        });
-    }
-    return transformed;
-}
-
-// simplify and transform projected coordinates for tile geometry
-function transform(points, type, z2, tx, ty) {
-    var newPoints = [],
-        tolerance = simplification / (tileSize * z2),
-        sqTolerance = tolerance * tolerance;
-
-    for (var i = 0, len = points.length; i < len; i++) {
-        var p = points[i];
-        // simplify, keeping points with significance > tolerance (plus 1st, last, and clip points on boundaries)
-        if (type === 1 || i === 0 || i === len - 1 || p[2] === -1 || p[2] > sqTolerance) {
-            newPoints.push(transformPoint(p, z2, tx, ty));
-        }
-    }
-    return newPoints;
-}
-
-function transformPoint(p, z2, tx, ty) {
-    var x = Math.round(tileSize * (p[0] * z2 - tx)),
-        y = Math.round(tileSize * (p[1] * z2 - ty));
-    return [x, y];
-}
-
-function toID(z, x, y) {
-    return (((1 << z) * y + x) * 32) + z;
-}
-
-function intersectX(a, b, x) { return [x, (x - a[0]) * (b[1] - a[1]) / (b[0] - a[0]) + a[1], -1]; }
-function intersectY(a, b, y) { return [(y - a[1]) * (b[0] - a[0]) / (b[1] - a[1]) + a[0], y, -1]; }
 
 function isClippedSquare(features) {
     if (features.length > 1) return false;
@@ -128,6 +85,18 @@ function isClippedSquare(features) {
         if (p[1] !== minPx && p[1] !== maxPx) return false;
     }
     return true;
+}
+
+function toID(z, x, y) {
+    return (((1 << z) * y + x) * 32) + z;
+}
+
+function intersectX(a, b, x) {
+    return [x, (x - a[0]) * (b[1] - a[1]) / (b[0] - a[0]) + a[1], -1];
+}
+
+function intersectY(a, b, y) {
+    return [(y - a[1]) * (b[0] - a[0]) / (b[1] - a[1]) + a[0], y, -1];
 }
 
 function coordsNumWithin(features, k) {
