fdadf7707801c7c86088300759291f5932b63a70
Vladimir Agafonkin <agafonkin@gmail.com>
2015-03-19 17:32:26 +0200

    eslint fixes

--- src/index.js
+++ src/index.js
@@ -185,21 +185,21 @@ function transformTile(tile, extent) {
 
     var z2 = tile.z2,
         tx = tile.x,
-        ty = tile.y;
+        ty = tile.y,
+        i, j, k;
 
-    for (var i = 0; i < tile.features.length; i++) {
+    for (i = 0; i < tile.features.length; i++) {
         var feature = tile.features[i],
             geom = feature.geometry,
-            type = feature.type,
-            transformed = [];
+            type = feature.type;
 
         if (type === 1) {
-            for (var j = 0; j < geom.length; j++) geom[j] = transformPoint(geom[j], extent, z2, tx, ty);
+            for (j = 0; j < geom.length; j++) geom[j] = transformPoint(geom[j], extent, z2, tx, ty);
 
         } else {
-            for (var j = 0; j < geom.length; j++) {
+            for (j = 0; j < geom.length; j++) {
                 var ring = geom[j];
-                for (var k = 0; k < ring.length; k++) ring[k] = transformPoint(ring[k], extent, z2, tx, ty);
+                for (k = 0; k < ring.length; k++) ring[k] = transformPoint(ring[k], extent, z2, tx, ty);
             }
         }
     }
@@ -213,7 +213,7 @@ function transformPoint(p, extent, z2, tx, ty) {
     var x = Math.round(extent * (p[0] * z2 - tx)),
         y = Math.round(extent * (p[1] * z2 - ty));
     return [x, y];
-};
+}
 
 // checks whether a tile is a whole-area fill after clipping; if it is, there's no sense slicing it further
 function isClippedSquare(features, extent, buffer) {
