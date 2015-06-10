d76964440567df64ae7cdf5477edcb9107e5cbe5
Vladimir Agafonkin <agafonkin@gmail.com>
2015-06-10 12:48:10 -0400

    add a functional test, close #13

---
+++ test/gen-tiles.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var geojsonvt = require('../src/index');
+
+module.exports = genTiles;
+
+
+if (require.main === module) { // if called directly
+
+    var fs = require('fs');
+
+    var result = genTiles(JSON.parse(fs.readFileSync(process.argv[2])), process.argv[3], process.argv[4]);
+
+    console.log(JSON.stringify(result));
+}
+
+function genTiles(data, maxZoom, maxPoints) {
+    var index = geojsonvt(data, {
+        indexMaxZoom: maxZoom,
+        indexMaxPoints: maxPoints
+    });
+
+    var output = {};
+
+    for (var id in index.tiles) {
+        var tile = index.tiles[id];
+        var z = Math.log(tile.z2) / Math.LN2;
+        output['z' + z + '-' + tile.x + '-' + tile.y] = index.getTile(z, tile.x, tile.y).features;
+    }
+
+    return output;
+}
---
+++ test/test-full.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var test = require('tape');
+var fs = require('fs');
+var path = require('path');
+var genTiles = require('./gen-tiles');
+
+testTiles('us-states.json', 'us-states-tiles.json', 7, 200);
+
+function testTiles(inputFile, expectedFile, maxZoom, maxPoints) {
+    test('full tiling test: ' + inputFile, function (t) {
+        t.same(genTiles(getJSON(inputFile), maxZoom, maxPoints), getJSON(expectedFile));
+        t.end();
+    });
+}
+
+function getJSON(name) {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, '/fixtures/' + name)));
+}
