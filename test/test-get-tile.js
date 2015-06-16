1bf1ebf5490903c43adbd02d4c64fca5d499f7a8
Vladimir Agafonkin <agafonkin@gmail.com>
2015-06-16 17:55:14 -0400

    more test coverage

--- test/test-get-tile.js
+++ test/test-get-tile.js
@@ -5,9 +5,28 @@ var fs = require('fs');
 var path = require('path');
 var geojsonvt = require('../src/index');
 
-test('getTile: us-states.json, z7-37-48', function (t) {
-    var index = geojsonvt(getJSON('us-states.json'));
-    t.same(index.getTile(7, 37, 48).features, getJSON('us-states-z7-37-48.json'));
+var square = [{
+    geometry: [[[-64, 4160], [-64, -64], [4160, -64], [4160, 4160], [-64, 4160]]],
+    type: 3,
+    tags: {name: 'Pennsylvania', density: 284.3}
+}];
+
+test('getTile: us-states.json', function (t) {
+    var log = console.log;
+
+    console.log = function () {};
+    var index = geojsonvt(getJSON('us-states.json'), {debug: 2});
+
+    var tile1 = index.getTile(7, 37, 48).features,
+        tile2 = index.getTile(9, 148, 192).features,
+        tile3 = index.getTile(11, 592, 768).features;
+
+    console.log = log;
+
+    t.same(tile1, getJSON('us-states-z7-37-48.json'), 'z7-37-48');
+
+    t.same(tile2, square, 'z9-148-192 (clipped square)');
+    t.same(tile3, square, 'z11-592-768 (clipped square)');
     t.end();
 });
 
