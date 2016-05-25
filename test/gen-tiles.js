aa3741ef246b5a02b7afc3943c4a282c33afdd08
Vladimir Agafonkin <agafonkin@gmail.com>
2016-05-25 13:12:31 +0300

    update deps

--- test/gen-tiles.js
+++ test/gen-tiles.js
@@ -1,16 +1,12 @@
 'use strict';
 
+var fs = require('fs');
 var geojsonvt = require('../src/index');
 
 module.exports = genTiles;
 
-
 if (require.main === module) { // if called directly
-
-    var fs = require('fs');
-
     var result = genTiles(JSON.parse(fs.readFileSync(process.argv[2])), process.argv[3], process.argv[4]);
-
     console.log(JSON.stringify(result));
 }
 
