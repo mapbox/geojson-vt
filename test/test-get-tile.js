7226705d9ae8d140243b11894bc9db0f89051ef8
John Firebaugh <john.firebaugh@gmail.com>
2018-05-03 00:03:52 -0700

    With buffer: 0, geometry coincident with a tile edge is clipped inconsistently (#102)
    * Update esm
    
    * Add tests showing inconsistency in bufferless clipping on tile edges
    
    * clip consistently (inclusively on top/left and exclusively on bottom/right)

--- test/test-get-tile.js
+++ test/test-get-tile.js
@@ -17,8 +17,6 @@ test('getTile: us-states.json', function (t) {
     console.log = function () {};
     var index = geojsonvt(getJSON('us-states.json'), {debug: 2});
 
-    console.log = log;
-
     t.same(index.getTile(7, 37, 48).features, getJSON('us-states-z7-37-48.json'), 'z7-37-48');
 
     t.same(index.getTile(9, 148, 192).features, square, 'z9-148-192 (clipped square)');
@@ -28,8 +26,36 @@ test('getTile: us-states.json', function (t) {
     t.equal(index.getTile(-5, 123.25, 400.25), null, 'invalid tile');
     t.equal(index.getTile(25, 200, 200), null, 'invalid tile');
 
+    console.log = log;
+
     t.equal(index.total, 37);
 
+    t.end();
+});
+
+test('getTile: unbuffered tile left/right edges', function (t) {
+    var index = geojsonvt({
+        type: 'LineString',
+        coordinates: [[0, 90], [0, -90]]
+    }, {
+        buffer: 0
+    });
+
+    t.same(index.getTile(2, 1, 1), null);
+    t.same(index.getTile(2, 2, 1).features, [{geometry: [[[0, 0], [0, 4096]]], type: 2, tags: null}]);
+    t.end();
+});
+
+test('getTile: unbuffered tile top/bottom edges', function (t) {
+    var index = geojsonvt({
+        type: 'LineString',
+        coordinates: [[-90, 66.51326044311188], [90, 66.51326044311188]]
+    }, {
+        buffer: 0
+    });
+
+    t.same(index.getTile(2, 1, 0).features, [{geometry: [[[0, 4096], [4096, 4096]]], type: 2, tags: null}]);
+    t.same(index.getTile(2, 1, 1).features, []);
     t.end();
 });
 
